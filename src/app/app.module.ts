797d41925f7152f76e479ccd2966fe9887f7636c
AkshayHere <akshaymohanrocks@gmail.com>
2021-02-20 03:58:04 +0800

    Initial Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-bar',
+  templateUrl: './tool-bar.component.html',
+  styleUrls: ['./tool-bar.component.css']
+})
+
+export class ToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
