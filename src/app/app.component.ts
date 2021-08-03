35fa77cbbb03429af9fcba382400b450775a45e8
Dude <akshymohn@gmail.com>
2021-08-03 17:33:31 +0800

    Just making some commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Movie Maniacs';
+  sayHelloId = 'sayHelloId';
+  fontColor = 'red';
+  canEdit = false;
+
+  random_rgba(): string {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += Math.floor(Math.random() * 10);
+    }
+    return color;
+  }
+
+  ngOnInit(){
+  }
+
+  onEditClick(){
+    this.canEdit = !this.canEdit;
+  }
+
+  sayMessage() {
+    this.fontColor = this.random_rgba();
+}
 }
